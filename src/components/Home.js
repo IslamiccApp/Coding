8f9cf1a25a8fbae6dc33143ae216b92e89b7674e
Erkan <erkankolakan@gmail.com>
2023-05-12 16:04:21 +0300

    btn_duzenlendi

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,16 +2,19 @@ import React from 'react'
 import resim from '../img/sayac_cerceve.png'
 import cerceve from '../img/button_cerceve.png'
 import el from '../img/zikir_el.png'
+import favicon from '../img/favicon.jpg'
 
 
 
 function Home() {
+  const [count, setCount] = React.useState(0);
+  <link rel="shortcut icon" type={favicon} href={favicon} /> //icon!!!!!!!!!!!!!!
   return (
     <div>
        <div className='container' >
       <div className='container_text'>
         <h2>Günün Hadisi</h2>
-      <span>
+      <span className='text'>
       <p>
       (Allah Rasûlü) “Din nasihattır/samimiyettir” buyurdu. “Kime Yâ Rasûlallah?” diye sorduk. O da; “Allah’a, Kitabına, Peygamberine, Müslümanların yöneticilerine ve bütün müslümanlara” diye cevap verdi. (Müslim, İmân, 95.)
       </p>
@@ -20,14 +23,14 @@ function Home() {
     </div>
     <div className='sayac'>
       <div className='sayac_ekran'>
-        <span> 9999 </span>
+        <span> {count} </span>
 
 
           <img src={resim} alt="sayac_cerceve"/>
       </div>
       </div>
       <div className='button'>
-        <button>
+        <button onClick={()=>setCount(count+1)}>
           <img src={cerceve} alt='btn_cerceve' />
           <img className='el' src={el} alt='zikir çeken el' />
         </button>
