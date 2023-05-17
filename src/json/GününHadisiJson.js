8c947641d78b037c10f130ddb86a7b900f394038
Erkan <erkankolakan@gmail.com>
2023-05-17 19:21:43 +0300

    noktalar_yapildi

--- src/json/GününHadisiJson.js
+++ src/json/GününHadisiJson.js
@@ -1,33 +1,33 @@
-const GününHadisi = [
-"pazartesi" =
+const GününHadisi = [{
+pazartesi :
 {
     "name" : "Pazartesi Duası",
     "meal" : `" Allah'ım haftaya başladığımız bu yeni günde senden hayır diliyoruz. İşimize ve hayatımıza tüm rahmetinle sahip çık biz inanan kullarını koru ve kolla. Biz her gün olduğu gibi bugün de şerden ve kötülükten sana sığınırız. Bizleri doğru yola sok ve o yolda yürümeyi nasip et."`
 },
-"sali" =  {
+sali :  {
     "name" : "Salı Hadisi",
     "meal" : `Hz. Aişe (ra) şöyle rivayet etmiştir: "Peygamber Efendimiz (s.a.v) salı ve çarşamba günlerinde oruç tutmayı sık sık yerine getirirdi." (Buhari, Müslim)`
 },
-"carsamba" = {
+carsamba : {
     "name" : "Çarşamba Hadisi",
     "meal" : `Hz. Câbir (r.a.) şöyle anlatmıştır: "Resulullah (s.a.v.) şöyle buyurdu: 'Allah, her çarşamba gece yarısından sonra semaya iner ve şöyle der: 'Benden mağfiret dileyen var mı ki onu mağfiret edeyim? Dua eden var mı ki ona icabet edeyim? İsteyen var mı ki ona verelim?' Bu durum sabaha kadar devam eder." (Tirmizi)`
 },
-"persembe" = {
+persembe : {
     "name" : "Perşembe Hadisi",
     "meal" : `"Pazartesi ve perşembe günleri cennet kapıları açılır. Din kardeşi ile aralarında düşmanlık bulunan kişi dışında Allah'a şirk koşmayan her kulun günahları bağışlanır. (Meleklere) siz şu iki kişiyi birbiriyle barışıncaya kadar tehir edin, evet siz bunları birbiriyle barışıncaya kadar tehir edin! buyurulur." (Müslim, Birr 34-36. Ayrıca bk. Ebû Dâvûd, Edeb 47)`
 },
-"cuma" = {
+cuma : {
     "name" : "Cuma Hadisi",
     "meal" : `“Üzerine güneşin doğduğu en hayırlı gün cuma günüdür; Âdem o gün yaratılmış, o gün cennete girmiş ve o gün cennetten çıkarılmıştır. Kıyamet de cuma günü kopacaktır.” (Müslim, Cuma, 18)`
 },
-"cumartesi" = {
+cumartesi : {
     "name" : "Cumartesi Hadisi",
     "meal" : `“Allah, toprağı cumartesi günü yarattı. Oradaki dağları pazar günü, ağaçları pazartesi günü, sevilmeyen şeyleri salı günü, nûru çarşamba günü yarattı. Hayvanları yeryüzüne perşembe günü yayıp dağıttı. Âdem’i yaratılanların sonuncusu olarak cuma gününün son saatlerinde, ikindiyle akşam arasında yarattı.”`
 },
-"pazar" = {
+pazar : {
     "name" : "Pazar Hadisi",
     "meal" : `Hz. Enes (r.a.) şöyle anlatmıştır: "Resulullah (s.a.v.) şöyle buyurdu: 'Pazar günü güneş doğduğunda, her çocuk ve yetişkin Müslüman'ın yanında (namaz kılabileceği) iki rekat namaz kılması sünnettir.'" (Tirmizi)`
-}
+}}
 ]
 
 export default GününHadisi
