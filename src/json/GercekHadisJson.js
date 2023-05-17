05d6d86ee3f73530424fa5a30aef201eef8d1d72
Erkan <erkankolakan@gmail.com>
2023-05-17 17:27:34 +0300

    hadis_eklendi

--- src/json/GercekHadisJson.js
+++ src/json/GercekHadisJson.js
@@ -1,70 +1,125 @@
 const gercekHadisJson = [
     {
-      "name": "إِنَّمَا الْأَعْمَالُ بِالنِّيَّةِ",
-      "meal": "Ameller niyetlere göredir."
-    },
-    {
-      "name": "البخيل من ذكرتُ عنده فلم يصلِّ عليَّ",
-      "meal": "Beni anıp üzerime salavat getirmeyen kimse cimridir."
-    },
-    {
-      "name": "مَنْ سَلَكَ طَرِيقًا يَلْتَمِسُ فِيهِ عِلْمًا سَهَّلَ اللَّهُ لَهُ بِهِ طَرِيقًا إِلَى الْجَنَّةِ",
-      "meal": "Kim ilim öğrenmek için bir yol tutarsa, Allah onun için cennete giden bir yol kolaylaştırır."
-    },
-    {
-      "name": "مَنْ لَا يَشْكُرُ النَّاسَ لَا يَشْكُرُ اللَّهَ",
-      "meal": "Kim insanlara şükretmezse, Allah'a da şükretmez."
-    },
-    {
-      "name": "المسلم من سلم المسلمون من لسانه ويده",
-      "meal": "Müslüman, diğer Müslümanların dilinden ve elinden güvende olduğu kişidir."
+      "name": "الدُّنْيَا كُلُّهَا مَتَاعٌ، وَخَيْرُ مَتَاعِ الدُّنْيَا الْمَرْأَةُ الصَّالِحَةُ.",
+      "meal": ` "Dünya, tamamen bir geçimlik malzemesidir. Ve dünyadaki en iyi geçimlik malzemesi, salih bir kadındır." (Müslim)`
     }
     ,
     {
-      "name": "",
-      "meal": ""
+      "name": `الصَّدَاقَةُ حَلَاوَةُ الْإِيمَانِ وَثَمَرَةُ الْجَنَّةِ.`,
+      "meal": ` "Dostluk, imanın tatlılığı ve cennetin meyvesidir." (Tirmizi)"`
     }
     ,
     {
-      "name": "",
-      "meal": ""
+      "name": `مَنْ أَحَبَّ لِلَّهِ وَأَبْغَضَ لِلَّهِ وَأَعْطَى لِلَّهِ وَمَنَعَ لِلَّهِ، فَقَدِ اسْتَكْمَلَ الْإِيمَانَ.`,
+      "meal": `"Kim Allah için sevdi, Allah için buğzetti, Allah için verdi ve Allah için alıkoyma yaptıysa, o imanını tamamlamıştır." (Ebu Davud)`
     }
     ,
     {
-      "name": "",
-      "meal": ""
+      "name": `مَنْ يُرِدِ اللَّهُ بِهِ خَيْرًا يُفَقِّهْهُ فِي الدِّينِ.`,
+      "meal": ` "Allah'ın hayır dilediği bir kişiyi din konusunda anlayışlı kılar." (Buhari)`
     }
     ,
     {
-      "name": "",
-      "meal": ""
+      "name": `الْبَيَانُ فِي السُّكُوتِ حَكْمٌ.`,
+      "meal": `"Susmakta bir hikmet vardır." (Tirmizi)`
     }
     ,
     {
-      "name": "",
-      "meal": ""
+      "name": `الْمُسْلِمُ مَنْ سَلِمَ الْمُسْلِمُونَ مِنْ لِسَانِهِ وَيَدِهِ.`,
+      "meal": `"Müslüman, dişi ve eliyle diğer Müslümanları zarardan koruyandır." (Buhari)`
     }
     ,
     {
-      "name": "",
-      "meal": ""
+      "name": `أَحَبُّ الْأَعْمَالِ إِلَى اللَّهِ أَدْوَمُهَا وَإِنْ قَلَّ.`,
+      "meal": `"Allah'a en sevimli olan işler, az olsa bile devamlı yapılanlardır." (Buhari)
+      `
     }
     ,
     {
-      "name": "",
-      "meal": ""
+      "name": `لَا يَدْخُلُ الْجَنَّةَ قَاطِعٌ.`,
+      "meal": ` "Kimse, başkalarıyla ilişkisini kesen cennete giremez." (Buhari)`
     }
     ,
     {
-      "name": "",
-      "meal": ""
+      "name": `الْمُؤْمِنُ مِرْآةُ أَخِيهِ.`,
+      "meal": `"Mümin, kardeşinin aynasıdır." (Ebu Davud)`
     }
     ,
     {
-      "name": "",
-      "meal": ""
+      "name": `إِذَا جَاءَ رَمَضَانُ فُتِّحَتْ أَبْوَابُ الْجَنَّةِ وَغُلِّقَتْ أَبْوَابُ النَّارِ.`,
+      "meal": `"Ramazan ayı geldiğinde cennet kapıları açılır, cehennem kapıları kapanır." (Buhari)`
     }
-    
+    ,
+    {
+      "name": `الدُّنْيَا مَلْعُونَةٌ مَلْعُونٌ مَا فِيهَا، إِلَّا ذِكْرُ اللَّهِ وَمَا وَالَاهُ، وَعَالِمًا وَمُتَعَلِّمًا.`,
+      "meal": `"Dünya, içindekileriyle lanetlenmiştir, sadece Allah'ın zikri ve ona yaklaşanlar, bilen ve öğrenen kişi hariç." (Tirmizi)`
+    }
+    ,
+    {
+      "name": `مَا زَالَ جِبْرِيلُ يُوصِينِي بِالْجَارِ حَتَّى ظَنَنْتُ أَنَّهُ سَيُوَرِّثُهُ.`,
+      "meal": `"Cibril bana komşuyu o kadar öğütledi ki, komşuyu mirasçı yapacağını sandım." (Buhari)`
+    } ,
+    {
+      "name": `لَا يُؤْمِنُ أَحَدُكُمْ حَتَّى يُحِبَّ لِأَخِيهِ مَا يُحِبُّ لِنَفْسِهِ. `,
+      "meal": `"Hiçbiriniz, kendisi için istediği gibi kardeşi için de istemedikçe gerçek iman etmiş olmaz." (Buhari)`
+    }
+    ,
+    {
+      "name": `قَالَ رَسُولُ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ: "مَنْ كَانَ يُؤْمِنُ بِاللَّهِ وَالْيَوْمِ الْآخِرِ فَلَا يُؤْذِي جَارَهُ".`,
+      "meal": `"Allah'a ve ahiret gününe inanan kişi, komşusuna zarar vermez." (Buhari)`
+    }
+    ,
+    {
+      "name": `لَا يُؤْمِنُ أَحَدُكُمْ حَتَّى يُحِبَّ لأَخِيهِ مَا يُحِبُّ لِنَفْسِهِ مِنَ الْخَيْرِ.`,
+      "meal": `"Hiçbiriniz, kendisi için istediği gibi kardeşi için de istemedikçe gerçek iman etmiş olmaz." (Buhari)`
+    }
+    ,
+    {
+      "name": `قَالَ رَسُولُ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ: "إِنَّ مِنْ حُسْنِ إِسْلَامِ الْمَرْءِ تَرْكَهُ مَا لَا يَعْنِيهِ". `,
+      "meal": `"Bir kişinin İslam'ının güzelliği, kendisine bir faydası olmayan şeyi terk etmesidir." (Tirmizi)`
+    }
+    ,
+    {
+      "name": `الْمُؤْمِنُ الْقَوِيُّ خَيْرٌ وَأَحَبُّ إِلَى اللَّهِ مِنَ الْمُؤْمِنِ الضَّعِيفِ وَفِي كُلٍّ خَيْرٌ.`,
+      "meal": `"Güçlü mümin, zayıf müminden daha hayırlı ve Allah katında daha sevimlidir. Her ikisinde de hayır vardır." (Müslim)`
+    }
+    ,
+    {
+      "name": `الْمُسْلِمُ أَخُو الْمُسْلِمِ، لَا يَظْلِمُهُ وَلَا يُسْلِمُهُ.`,
+      "meal": `"Mümin, diğer müminin kardeşidir. Ona zulmetmez ve onu düşmana teslim etmez." (Müslim)`
+    }
+    ,
+    {
+      "name": `لَا يُؤْمِنُ أَحَدُكُمْ حَتَّى يَحْبَ لِأَخِيهِ مَا يَحْبُ لِنَفْسِهِ.`,
+      "meal": `"Biriniz gerçek anlamda iman etmiş olmaz, kendisi için istediği gibi kardeşi için de istemedikçe." (Buhari)`
+    }
+    ,
+    {
+      "name": `قَالَ رَسُولُ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ: "مَنْ رَأَى مِنْكُمْ مُنْكَرًا فَلْيُغَيِّرْهُ بِيَدِهِ، فَإِنْ لَمْ يَسْتَطِعْ فَبِلِسَانِهِ، فَإِنْ لَمْ يَسْتَطِعْ فَبِقَلْبِهِ، وَذَلِكَ أَضْعَفُ الْإِيمَانِ".`,
+      "meal": `"Peygamber (s.a.v.) şöyle buyurdu: 'Sizden biri bir kötülük görürse onu eliyle değiştirsin, eliyle yapamazsa diliyle değiştirsin, dilini de kullanamazsa kalbiyle karşı çıksın, bu ise imanın en zayıf şeklidir.'" (Müslim)`
+    }
+    ,
+    {
+      "name": `قَالَ رَسُولُ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ: "إِذَا لَمْ تَسْتَحْيِ فَاصْنَعْ مَا شِئْتَ". `,
+      "meal": `"Peygamber (s.a.v.) şöyle buyurdu: 'Hayâ duygusu taşımıyorsan, dilediğini yap.'" (Buhari)`
+    }
+    ,
+    {
+      "name": `قَالَ رَسُولُ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ: "كُلُّ أَمَّتِي يَدْخُلُونَ الْجَنَّةَ إِلَّا مَنْ أَبَى".`,
+      "meal": `"Peygamber (s.a.v.) şöyle buyurdu: 'Bütün ümmetim cennete girecek, kabul etmeyen hariç.'" (Buhari)`
+    }
+    ,
+    {
+      "name": `قَالَ رَسُولُ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ: "مَنْ كَانَ يُؤْمِنُ بِاللَّهِ وَالْيَوْمِ الْآخِرِ فَلَا يُؤْذِي جَارَهُ".`,
+      "meal": `"Peygamber (s.a.v.) şöyle buyurdu: 'Allah'a ve ahiret gününe inanan kişi, komşusuna zarar vermez.'" (Buhari)`
+    }
+    ,
+    {
+      "name": `قَالَ رَسُولُ اللَّهِ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ: "إِنَّ اللَّهَ يُحِبُّ إِذَا عَمِلَ أَحَدُكُمْ عَمَلاً أَنْ يُتْقِنَهُ".`,
+      "meal": `"Peygamber (s.a.v.) şöyle buyurdu: 'Allah, sizden birinin bir iş yaparsa onu iyi bir şekilde yapmasını sever.'" (Tirmizi)`
+    }
+
+
   
   ]
   export default gercekHadisJson
