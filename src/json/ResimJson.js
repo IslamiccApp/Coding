35d8e2826874192a311763b0e8c894a86284a49e
Erkan <erkankolakan@gmail.com>
2023-05-19 13:07:55 +0300

    sıkıştırılmış_resimler

--- src/json/ResimJson.js
+++ src/json/ResimJson.js
@@ -1,76 +1,76 @@
 const ResimJson=[
     {
-        resim:'https://images8.alphacoders.com/355/355006.jpg'
+        resim:'https://i.imgur.com/ChM58RM.jpg'
     }   
     ,
     {
-        resim:'https://images2.alphacoders.com/546/546575.jpg'
+        resim:'https://i.imgur.com/uGrGJAc.jpg'
     }   
  
     ,
     {
-        resim:'https://images6.alphacoders.com/522/522976.jpg'
+        resim:'https://i.imgur.com/Z8q1WMQ.jpg'
     }   
     ,
     {
-        resim:'https://images6.alphacoders.com/396/396884.jpg'
+        resim:'https://i.imgur.com/4NWRWOn.jpg'
     }   
     ,
     {
-        resim:'https://images5.alphacoders.com/719/719791.jpg'
+        resim:'https://i.imgur.com/lmlHwlw.jpg'
     }   
     ,
     {
-        resim:'https://images6.alphacoders.com/712/712639.jpg'
+        resim:'https://i.imgur.com/LvhECrN.jpg'
     }   
     ,
     {
-        resim:'https://images7.alphacoders.com/437/437635.jpg'
+        resim:'https://i.imgur.com/pqusOlS.jpg'
     }   
     ,
     {
-        resim:'https://images6.alphacoders.com/343/343678.jpg'
+        resim:'https://i.imgur.com/RwooR6J.jpg'
     }   
     ,
     {
-        resim:'https://images2.alphacoders.com/800/800595.jpg'
+        resim:'https://i.imgur.com/ieM0Ubo.jpg'
     }
     ,
     {
-        resim:'https://images5.alphacoders.com/372/372467.jpg'
+        resim:'https://i.imgur.com/mY2cH7E.jpg'
     }
     ,
     {
-        resim:'https://images5.alphacoders.com/545/545241.jpg'
+        resim:'https://i.imgur.com/hEPLb0p.jpg'
     }   
     ,
     {
-        resim:'https://images3.alphacoders.com/718/718925.jpg'
+        resim:'https://i.imgur.com/cBVRkbb.jpg'
     }   
     ,
     {
-        resim:'https://images3.alphacoders.com/804/804430.jpg'
+        resim:'https://i.imgur.com/OFc8lMR.jpg'
     }   
     
     ,
     {
-        resim:'https://images.alphacoders.com/925/925790.jpg'
+        resim:'https://i.imgur.com/NQdY0UW.jpg'
     }   
     ,
     {
-        resim:'https://images2.alphacoders.com/107/1074920.jpg'
+        resim:'https://i.imgur.com/69XzoKa.jpg'
     }   
     ,
     {
-        resim:'https://images6.alphacoders.com/687/687484.jpg'
+        resim:'https://i.imgur.com/DkKcTjw.jpg'
     }   
     ,
     {
-        resim:'https://images4.alphacoders.com/111/1116043.jpg'
+        resim:'https://i.imgur.com/RhOPiGH.jpg'
     }
     ,
     {
-        resim:'https://images6.alphacoders.com/773/773196.jpg'
+        resim:'https://i.imgur.com/RhOPiGH.jpg'
     }   
 
 
