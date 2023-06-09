a313f4dec42dcaf8e9957e4993b8e7bac6161026
Erkan <erkankolakan@gmail.com>
2023-06-09 21:09:59 +0300

    resimlere_hedislere_ekleme_yapildi

--- src/json/HadislerJson.js
+++ src/json/HadislerJson.js
@@ -1,109 +1,141 @@
 const HadislerJson=[
    {
+      hadisName:'Allah',
+      hadisMean:`Allah, kainatı yaratan, her şeye gücü yeten, doğmamış ve doğurmamış olan, kudret sahibi`,
+      Count:'',
+
+     },
+   {
     hadisName:'SubhanAllah',
-    hadisMean:'tüm olumsuz sıfatlardan uzak olarak kabul etmek, soyutlamak, tertemiz bilmek, paklamak, arındırmak, şanını yüceltmek gibi anlamlara sahiptir. Türkçe kelime karşılığı Allah Kusursuzdur, Allah Arınıktır veya Allah Mutlak-Üstündür şeklinde gösterilebilir.',
+    hadisMean:'Tüm olumsuz sıfatlardan uzak olarak kabul etmek, soyutlamak, tertemiz bilmek, paklamak, arındırmak, şanını yüceltmek gibi anlamlara sahiptir. Türkçe kelime karşılığı Allah Kusursuzdur, Allah Arınıktır veya Allah Mutlak-Üstündür şeklinde gösterilebilir.',
     Count:'',
-    id:"0"
    }
    ,
+   {
+      hadisName:'Hasbünallahü ve Ni’melvekîl',
+      hadisMean:`Allah bana yeter. O ne güzel vekildir.`,
+      Count:'',
+
+     },
    {
     hadisName:'Alhamdulillah',
     hadisMean:`Hamd, övgü Allah'a mahsustur." Allah'a şükran ve övgü ifade eder.'`,
     Count:'',
-    id:"1"
+
    }
    ,
    {
     hadisName:'Allahu Akbar',
     hadisMean:`"Allah en büyüktür." Allah'ın büyüklüğünü ve üstünlüğünü ifade eder`,
     Count:'',
-    id:"2"
+
    }
    ,
    {
     hadisName:'La ilahe illallah',
     hadisMean:`"Allah'tan başka ilah yoktur." Allah'ın birliğini ve tek ilah olduğunu ifade eder.`,
     Count:'',
-    id:"3"
+ 
    }
    ,
    {
     hadisName:'Astaghfirullah',
     hadisMean:` "Allah'tan bağışlanma dilerim." Allah'tan affını ve bağışlanmasını istemek için kullanılır.`,
     Count:'',
-    id:"4"
+
    }
    ,
    {
     hadisName:'La havle ve la kuvvete illa billah',
     hadisMean:`"Allah'tan başka güç ve kuvvet yoktur." Her türlü güç ve kudretin yalnızca Allah'a ait olduğunu ifade eder.`,
     Count:'',
-    id:"5"
+
    }
    ,
    {
     hadisName:'SubhanAllahi ve bihamdihi',
     hadisMean:`"Allah'ı tenzih ederim ve O'na hamdederim." Allah'ın kusursuzluğunu ve övgüye layık olduğunu ifade eder.`,
     Count:'',
-    id:"6"
+
    }
    ,
    {
     hadisName:'La ilahe illa anta subhanaka inni kuntu minaz-zalimin',
     hadisMean:`"Senden başka ilah yoktur, seni tesbih ederim; gerçekten ben zulmedenlerden oldum." Peygamber Yunus (Jonah)'un dua ettiği ve Allah'ın onu balığın karnından kurtardığı bir duadır.`,
     Count:'',
-    id:"7"
+
    } 
    ,
    {
+      hadisName:'Hasbünallahü ve Ni’melvekîl',
+      hadisMean:`Allah bana yeter. O ne güzel vekildir.`,
+      Count:'',
+
+     },
+     {
+      hadisName:'Estağfirullâhe Ve Etûbu İleyh',
+      hadisMean:`Allâh’tan mağfiret dilerim ve O’na tevbe ederim.`,
+      Count:'',
+
+     },
+     {
+      hadisName:'Sübhânallahi ve bi-hamdihî, estağfirullâhe ve etûbü ileyh',
+      hadisMean:`Hz. Aişe (ra)'un rivayetine göre, Efendimiz (SAV) sık sık şöyle derdi: “Sübhânallahi ve bi-hamdihî, estağfirullâhe ve etûbü ileyh. / Ben Allah’ı ulûhiyyet makamına yakışmayan sıfatlardan tenzih eder ve O’na hamd ederim. Allah’tan beni bağışlamasını diler ve günahlarıma tövbe ederim.” (Buharî, Ezan 123, 139; Müslim, Salat 218-220)`,
+      Count:'',
+
+     },
+     {
+      hadisName:'Lâ ilahe illallahü melikül Hakkul Mübin',
+      hadisMean:`Hz. Ali (r.a)’dan rivayetine göre Peygamberimiz (SAV) şöyle buyurmuştur;n“Bir kimse günde 100 defa ‘Lâ ilahe illallahü melikül Hakkul Mübin‘ derse, bu onun için fakirlikten eman ve kabir yalnızlığından kendisine yoldaş olur ve bununla zenginliği celb etmiş yani kendisine çekmiş ve Cennetin de kapısını çalmış olur” (Ramuz- elhadis)`,
+      Count:'',
+
+     },
+   {
     hadisName:'Ya Rahman',
-    hadisMean:`"Ey Merhametli." Allah'ın sonsuz merhametine seslenen bir ifadedir.
-    `,
+    hadisMean:`"Ey Merhametli." Allah'ın sonsuz merhametine seslenen bir ifadedir.`,
     Count:'',
-    id:"8"
+
    } 
    ,
    {
     hadisName:'Ya Rahim',
     hadisMean:`"Ey Bağışlayıcı." Allah'ın sonsuz bağışlamasına seslenen bir ifadedir.`,
     Count:'',
-    id:"9"
+
    } 
    ,
    {
     hadisName:'Ya Azim',
     hadisMean:`"Ey Büyük." Allah'ın yüceliğini ifade eden bir ifadedir`,
     Count:'',
-    id:"10"
+ 
    } 
    ,
    {
     hadisName:'Ya Halim',
     hadisMean:`"Ey Sabırlı." Allah'ın sonsuz sabrını ifade eden bir ifadedir.`,
     Count:'',
-    id:"11"
+
    } 
    ,
    {
     hadisName:'Ya Subhan',
     hadisMean:`"Ey Mukaddes." Allah'ın kutsallığını ifade eden bir ifadedir.`,
     Count:'',
-    id:"12"
+
    } 
    ,
    {
     hadisName:'Ya Kerim',
     hadisMean:` "Ey Cömert." Allah'ın cömertliğini ifade eden bir ifadedir.`,
     Count:'',
-    id:"13"
+ 
    } 
-   ,
-   {
-    hadisName:'Ya Allah',
-    hadisMean:` "Ey Allah." Allah'a doğrudan seslenen bir ifadedir.`,
-    Count:'',
-    id:"14"
-   }   
+
+
+    
+     
+     
    
 ]
 export default HadislerJson;
--- src/json/MobilResimDegistirJson.js
+++ src/json/MobilResimDegistirJson.js
@@ -1,5 +1,96 @@
 const mobilJson=[
     {
+        "resim":"  https://i.imgur.com/v3gRv9k.jpg"
+    }
+    ,
+    {
+        "resim":" https://i.imgur.com/HSulI0w.jpg "
+    }
+    ,
+    {
+        "resim":" https://i.imgur.com/Q2Kdhsy.jpg "
+    }
+    ,
+    {
+        "resim":" https://i.imgur.com/p7ebK5J.jpg"
+    }
+    ,
+    {
+        "resim":" https://i.imgur.com/2lBEft9.jpg"
+    }
+    ,
+    {
+        "resim":" https://i.imgur.com/MUjNROa.jpg"
+    }
+    ,
+    {
+        "resim":" https://i.imgur.com/16NlQhh.jpg"
+    }
+    ,
+    {
+        "resim":" https://i.imgur.com/HOl8wie.jpg"
+    }
+    ,
+    {
+        "resim":" https://i.imgur.com/EG07PgF.jpg"
+    }
+    ,
+    {
+        "resim":" https://i.imgur.com/Stb0ltL.jpg"
+    }
+    ,
+    {
+        "resim":" https://i.imgur.com/VRyiia3.jpg"
+    }
+    ,
+    {
+        "resim":" https://i.imgur.com/5PTUxkm.jpg"
+    }
+    ,
+    {
+        "resim":" https://i.imgur.com/yKO9I1d.jpg"
+    }
+    ,
+    {
+        "resim":" https://i.imgur.com/MUlAi3h.jpg"
+    }
+    ,
+    {
+        "resim":" https://i.imgur.com/LQld8ey.jpg"
+    }
+    ,
+ 
+    {
+        "resim":" https://i.imgur.com/0tQxjrI.jpg"
+    }
+    ,
+    {
+        "resim":" https://i.imgur.com/4tQomxU.jpg"
+    }
+    ,
+    {
+        "resim":" https://i.imgur.com/T5KdElN.jpg"
+    }
+    ,
+    {
+        "resim":" https://i.imgur.com/tmaMWNV.jpg"
+    }
+    ,
+    {
+        "resim":" https://i.imgur.com/PygDTyx.jpg"
+    }
+    ,
+    {
+        "resim":" https://i.imgur.com/NfVVSUg.jpg"
+    }
+    ,
+    {
+        "resim":" https://i.imgur.com/DVmyuTH.jpg"
+    }
+    
+    ,
+
+    {
         "resim":"https://i.imgur.com/6eBxmMW.jpg"
     }
     ,
@@ -59,10 +150,7 @@ const mobilJson=[
         "resim":"https://i.imgur.com/qLfp8Lb.jpg"
     }
     ,
-    {
-        "resim":"https://i.imgur.com/7Uj4icu.jpg"
-    }
-    ,
+ 
     {
         "resim":"https://i.imgur.com/SuLLrKn.jpg"
     }
@@ -93,41 +181,35 @@ const mobilJson=[
         "resim":"https://i.imgur.com/zt9whVx.jpg"
     }
 
+    
     // ,
     // {
     //     "resim":"https://images.pexels.com/photos/718646/pexels-photo-718646.jpeg?auto=compress&cs=tinysrgb&w=1600"
     // }
-
     // ,
     // {
     //     "resim":"https://images.pexels.com/photos/2449510/pexels-photo-2449510.jpeg?auto=compress&cs=tinysrgb&w=1600"
     // }
-
     // ,
     // {
     //     "resim":"https://images.pexels.com/photos/2784992/pexels-photo-2784992.jpeg?auto=compress&cs=tinysrgb&w=1600"
     // }
-
     // ,
     // {
     //     "resim":"https://images.pexels.com/photos/2370933/pexels-photo-2370933.jpeg?auto=compress&cs=tinysrgb&w=1600"
     // }
-
     // ,
     // {
     //     "resim":"https://images.pexels.com/photos/2325271/pexels-photo-2325271.jpeg?auto=compress&cs=tinysrgb&w=1600"
     // }
-
     // ,
     // {
     //     "resim":"https://images.pexels.com/photos/3540318/pexels-photo-3540318.jpeg?auto=compress&cs=tinysrgb&w=1600"
     // }
-
     // ,
     // {
     //     "resim":"https://images.pexels.com/photos/2319432/pexels-photo-2319432.jpeg?auto=compress&cs=tinysrgb&w=1600"
     // }
-
     // ,
     // {
     //     "resim":"https://images.pexels.com/photos/13733345/pexels-photo-13733345.jpeg?auto=compress&cs=tinysrgb&w=1600"
@@ -205,7 +287,6 @@ const mobilJson=[
     //     "resim":"https://images.pexels.com/photos/15545431/pexels-photo-15545431.jpeg?auto=compress&cs=tinysrgb&w=1600"
     // }
     // ,
-
     // {
     //     "resim":"https://images.pexels.com/photos/14209752/pexels-photo-14209752.jpeg?auto=compress&cs=tinysrgb&w=1600"
     // }
